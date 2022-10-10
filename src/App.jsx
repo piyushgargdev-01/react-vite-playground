f24629a253cba1f7f4ef5fcbb35c3be51efb3425
Piyush Garg Dev <piyushgargdev@Piyushs-MacBook-Pro.local>
2022-10-11 00:41:18 +0530

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,56 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+let isAdmin = TextTrackCue;
+
+const person = {
+  id: 1,
+  name: "Piyush Garg",
+  age: 22,
+  balance: 100,
+};
+
+const personProxy = new Proxy(person, {
+  set: (obj, prop, value) => {
+    if (prop == "name") {
+      obj[prop] = value;
+    } else {
+      throw new Error("You can only change name property");
+    }
+  },
+  get: (obj, prop) => {
+    if (prop == "balance") {
+      if (isAdmin) return obj[prop];
+      return null;
+    }
+    return obj[prop];
+  },
+});
 
 function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.jsx</code> and save to reload!
-				</p>
-				<span className="App-link">Hello from codedamn :)</span>
-			</header>
-		</div>
-	)
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>{personProxy.id}</h1>
+        <h1>{personProxy.name}</h1>
+        <h1>{personProxy.balance}</h1>
+        <button
+          onClick={(e) => {
+            personProxy.id = 2;
+            setState((s) => s + 1);
+          }}
+        >
+          Change
+        </button>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.jsx</code> and save to reload!
+        </p>
+        <span className="App-link">Hello from codedamn :)</span>
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;
