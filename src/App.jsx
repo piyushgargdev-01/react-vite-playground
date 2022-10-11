35112300be3aa5f694cbd5c7e5500e78b2b789ec
Piyush Garg Dev <piyushgargdev@Piyushs-MacBook-Pro.local>
2022-10-12 01:05:11 +0530

    Init

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,46 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.jsx</code> and save to reload!
-				</p>
-				<span className="App-link">Hello from codedamn :)</span>
-			</header>
-		</div>
-	)
+function withUnderline(Component) {
+  return (props) => {
+    return (
+      <Component
+        {...props}
+        style={{ textDecoration: "underline", color: "red" }}
+      />
+    );
+  };
 }
 
-export default App
+const Text = (props) => {
+  return <h1 {...props}>I am a Text</h1>;
+};
+
+const TextWithUnderline = withUnderline(Text);
+
+const Para = (props) => {
+  return <p {...props}>I am Para</p>;
+};
+
+const MyPara = withUnderline(Para);
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.jsx</code> and save to reload!
+        </p>
+        <Para />
+        <MyPara />
+        <span className="App-link">Hello from codedamn :)</span>
+        <Text />
+        <TextWithUnderline />
+      </header>
+    </div>
+  );
+}
+
+export default App;
