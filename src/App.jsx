9884a8341c21d749f9f8eccd03ebf24f1b972c86
Piyush Garg Dev <piyushgargdev@Piyushs-MacBook-Pro.local>
2022-10-11 22:07:31 +0530

    Init

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,47 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.jsx</code> and save to reload!
-				</p>
-				<span className="App-link">Hello from codedamn :)</span>
-			</header>
-		</div>
-	)
+const animalFun = {
+  walk: () => <p>Walking...</p>,
+  sleep: () => <p>Sleeping..</p>,
+};
+
+const dogFn = {
+  bark: () => <p>Barking...</p>,
+};
+
+class Dog {
+  constructor(name) {
+    this.name = name;
+  }
 }
 
-export default App
+class Lion {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+Object.assign(dogFn, animalFun);
+Object.assign(Dog.prototype, dogFn);
+Object.assign(Lion.prototype, animalFun);
+
+const tuffy = new Dog("Tuffy");
+
+function App() {
+  return (
+    <div className="App">
+      {tuffy.bark()}
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.jsx</code> and save to reload!
+        </p>
+        <span className="App-link">Hello from codedamn :)</span>
+      </header>
+    </div>
+  );
+}
+
+export default App;
